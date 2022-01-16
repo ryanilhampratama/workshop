b35926c947e626a64f26bc4083e2cda2555c280b
ryanilhampratama <97815594+ryanilhampratama@users.noreply.github.com>
2022-01-16 14:38:56 +0700

    workshop himaditi ryan

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">header</div>
+      <div className="section">
+        <div className=" card section-1">1</div>
+        <div className=" card section-2">2</div>
+        <div className=" card section-3">3</div>
+        <div className=" card section-4">4</div>
+        <div className=" card section-5">5</div>
+        <div className=" card section-6">6</div>
+      </div>
+
+      <div className="body">
+        <div className="Anak body-1">1</div>
+        <div className="Anak body-2">2</div>
+        <div>1</div>
+          <div>2</div>
+        </div>
+        <div className="Anak body-3">3</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
+    
+  );
+}
+
+export default App;
